bb4059e55c4780aba5cc12a10aba3d1ae2cf74f3
mongkyo <dreamong91@gmail.com>
2018-10-02 16:43:03 +0900

    try-except 구문을 사용해서 objects.get()에서 발생하는 DoesNotExist에외처리

--- app/polls/views.py
+++ app/polls/views.py
@@ -1,24 +1,31 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from django.shortcuts import render
+from django.template import loader
 
 from .models import Question
 
 
 def index(request):
-
     # Question 클래스에 대한 querySet을 가져옴
     # 게시일자 속성에 대한 내림차순 순서로 최대 5개까지
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {
+        'latest_question_list': latest_question_list,
+    }
 
-    # 가져온 Question QuerySet을 사용, 각 Question의 question_text속성값들을
-    # list comprehension을 사용해 리스트로 생성
-    # 생성한 리스트를 ', '문자열의 join메서드의 인수로 전달
-    # output에 쉼표단위로 연결된 문자열을 할당
-    output = ', '.join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+    return render(request, 'polls/index.html', context)
 
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404('Question does not exist')
+
+    context = {
+        'question': question,
+    }
+    return render(request, 'polls/detail.html', context)
 
 
 def results(request, question_id):
@@ -28,4 +35,3 @@ def results(request, question_id):
 
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
-
