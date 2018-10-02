e9bbf0c8bb8f3566b2a5f4b4f24724435b0f1807
mongkyo <dreamong91@gmail.com>
2018-10-02 16:00:40 +0900

    index, detail, result, vote view를 url과 연결, index뷰는 가장 최근에 발행된 Question을 5개까지 보여줌

--- app/polls/urls.py
+++ app/polls/urls.py
@@ -6,8 +6,13 @@ from . import views
 urlpatterns = [
     # /polls/       -> config/urls.py에 polls/로 왔을 때 polls/urls.py에서 처리한다고 되어있기 때문
     # name='index'를 통해 네이밍을 해주었기 때문에 config에서 index를 받아온다
+
+    # r'^$'
     path('', views.index, name='index'),
+    # r'^(?P<question_id>\d+)/$'
     path('<int:question_id>/', views.detail, name='detail'),
+    # r'^(?P<question_id>\d+)/results/$'
     path('<int:question_id>/results/', views.results, name='results'),
+    # r'^(?P<question_id>\d+)/vote/$
     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
--- app/polls/views.py
+++ app/polls/views.py
@@ -1,18 +1,31 @@
 from django.http import HttpResponse
 
+from .models import Question
+
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the poll index")
+
+    # Question 클래스에 대한 querySet을 가져옴
+    # 게시일자 속성에 대한 내림차순 순서로 최대 5개까지
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+
+    # 가져온 Question QuerySet을 사용, 각 Question의 question_text속성값들을
+    # list comprehension을 사용해 리스트로 생성
+    # 생성한 리스트를 ', '문자열의 join메서드의 인수로 전달
+    # output에 쉼표단위로 연결된 문자열을 할당
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
 
 
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
 
 
-def result(request, question_id):
+def results(request, question_id):
     response = "You're looking at the results of questions %s."
     return HttpResponse(response % question_id)
 
 
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
+
