ef4e9483b2caa09ed7244927aa55bf51fb762c23
mongkyo <dreamong91@gmail.com>
2018-10-04 11:13:08 +0900

    detail template구현 및 polls.urls에 namespace지정

--- app/polls/urls.py
+++ app/polls/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 
+app_name = 'polls'
 urlpatterns = [
     # /polls/       -> config/urls.py에 polls/로 왔을 때 polls/urls.py에서 처리한다고 되어있기 때문
     # name='index'를 통해 네이밍을 해주었기 때문에 config에서 index를 받아온다
