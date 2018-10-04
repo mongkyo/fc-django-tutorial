6bb07eed01cd176539c32467f7562d9262c7725b
mongkyo <dreamong91@gmail.com>
2018-10-04 13:03:59 +0900

    IndexView로 /polls/cbv/구현

--- app/polls/urls.py
+++ app/polls/urls.py
@@ -1,9 +1,14 @@
-from django.urls import path
+from django.urls import path, include
 
-from .views import fbv as views
+from .views import cbv, fbv as views
 
 
 app_name = 'polls'
+
+urlpatterns_cbv = [
+    path('', cbv.IndexView.as_view(), name='index'),
+]
+
 urlpatterns = [
     # /polls/       -> config/urls.py에 polls/로 왔을 때 polls/urls.py에서 처리한다고 되어있기 때문
     # name='index'를 통해 네이밍을 해주었기 때문에 config에서 index를 받아온다
@@ -16,4 +21,8 @@ urlpatterns = [
     path('<int:question_id>/results/', views.results, name='results'),
     # r'^(?P<question_id>\d+)/vote/$
     path('<int:question_id>/vote/', views.vote, name='vote'),
+
+    # /polls/cbv/로 시작하는 URL요청은
+    # 위의 urlpatterns_dbv리스트 내의 내용에서 처리
+    path('cbv/', include(urlpatterns_cbv))
 ]
