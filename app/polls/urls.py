31aa251500408cf887798532613e69015ee201ae
mongkyo <dreamong91@gmail.com>
2018-10-04 14:53:13 +0900

    ResultsView, VoteView추가

--- app/polls/urls.py
+++ app/polls/urls.py
@@ -4,12 +4,12 @@ from .views import cbv, fbv as views
 
 
 app_name = 'polls'
-
 urlpatterns_cbv = [
     path('', cbv.IndexView.as_view(), name='index'),
-    path('<int:question_id>/', cbv.DetailView.as_view(), name='detail')
+    path('<int:question_id>/', cbv.DetailView.as_view(), name='detail'),
+    path('<int:question_id>/results', cbv.ResultsViews.as_view(), name='results'),
+    path('<int:question_id>/vote', cbv.VoteView.as_view(), name='vote')
 ]
-
 urlpatterns = [
     # /polls/       -> config/urls.py에 polls/로 왔을 때 polls/urls.py에서 처리한다고 되어있기 때문
     # name='index'를 통해 네이밍을 해주었기 때문에 config에서 index를 받아온다
