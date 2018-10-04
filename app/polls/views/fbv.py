1d765b5bd7a08a6ee135416b1611a4bf351acc08
mongkyo <dreamong91@gmail.com>
2018-10-04 12:42:55 +0900

    views모듈의 내용을 views.fbv로 이동 및 urls의 import부분 변경

--- app/polls/views/fbv.py
+++ app/polls/views/fbv.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, get_object_or_404
 from django.template import loader
 from django.urls import reverse
 
-from .models import Question, Choice
+from ..models import Question, Choice
 
 
 def index(request):
@@ -30,8 +30,14 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-    response = "You're looking at the results of questions %s."
-    return HttpResponse(response % question_id)
+    # question_id에 해당하는 Question인스턴스를 render함수의 context로 전달
+    # template은 'polls/result.html'을 사용
+    context = {
+        'question': get_object_or_404(Question, pk=question_id)
+    }
+    # Template에서는 전달받은 Question인스턴스에 속하는 Choice목록을 순회하며 보여줌
+    # 이 때, 각 Choice아이템들의 'choice_text' 및 'votes'속성값도 같이 출력,
+    return render(request, 'polls/results.html', context)
 
 
 def vote(request, question_id):
